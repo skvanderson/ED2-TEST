b89a457f17a5a622b5b1a14a03fcaac69de7afbf
Sharlles Andersonn <54425710+skvanderson@users.noreply.github.com>
2024-03-11 05:45:32 -0300

    Update Q_04.c
    Simplificando as questões

--- Q_04.c
+++ Q_04.c
@@ -1,28 +1,27 @@
-//questão 04
+// questão 04
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *decimalToRoman(int num) {
+char *decimal_para_romano(int num) {
+char *romanos[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  int valor[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  char *numero_romano = malloc(50);
+    numero_romano[0] = '\0';
 
-  char *romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-  int decimalValues[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-  char *romanNumber = malloc(50);
-  romanNumber[0] = '\0';
-  for (int i = 0; i < 13; i++) {
-    if (num >= decimalValues[i]) {
-      strcat(romanNumber, romanNumerals[i]);
-      num -= decimalValues[i];
+    for (int i = 0; i < 13; i++) {
+        while (num >= valor[i]) {
+            strcat(numero_romano, romanos[i]);
+            num -= valor[i];
+        }
     }
-  }
-  return romanNumber;
+    return numero_romano;
 }
 
 int main() {
-  for (int i = 1; i <= 100; i++) {
-    printf("%d - %s\n", i, decimalToRoman(i));
-  }
-
-  return 0;
+    for (int i = 1; i <= 100; i++) {
+        printf("%d - %s\n", i, decimal_para_romano(i));
+    }
+    return 0;
 }
