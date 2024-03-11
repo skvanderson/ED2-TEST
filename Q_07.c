098b0b85b1d2a527ede97e9133a50bc5fc65c105
Sharlles Andersonn <54425710+skvanderson@users.noreply.github.com>
2024-03-11 04:40:51 -0300

    Update Q_07.c
    Correção de erros de lógicas

--- Q_07.c
+++ Q_07.c
@@ -1,26 +1,31 @@
-//questão07
-
 #include <stdio.h>
 
-int isPrime(int numero, int v) {
-    return v == 1 ? 1 : (numero % v == 0 ? 0 : isPrime(numero, v - 1));
+int Primo(int n, int divisor) {
+    if (divisor == 1) {
+        return 1;
+    }
+    
+    if (n % divisor == 0) {
+        return 0;
+    }
+    return Primo(n, divisor - 1);
 }
 
 int main() {
-    
     int num;
-    printf("Digite um número ");
+    printf("Digite um número inteiro positivo\n");
     scanf("%i", &num);
-
+    
     if (num <= 1) {
-        printf("%i não é primo.\n",num);
+        printf("%i não é um número primo.\n", num);
     } else {
-        if (isPrime(num, num / 2)) {
-            printf("%i é primo.\n",num);
+        int result = Primo(num, num - 1);
+        if (result) {
+            printf("%i é um número primo.\n", num);
         } else {
-            printf("%i não é primo.\n",num);
+            printf("%i não é um número primo.\n", num);
         }
     }
-
+    
     return 0;
 }
